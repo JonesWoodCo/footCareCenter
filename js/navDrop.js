c10fa1ed75c3fa323714d14193161a96a59f2df6
Topher <cjones.wingsofgold@gmail.com>
2015-02-24 11:30:10 -0600

    started jQuery to toggle nav drop downs

---
+++ js/navDrop.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+         $('.option1').hide();
+
+
+  $('.libraryHover').mouseenter(function() {
+    $('.option1').toggle();
+  
+  });
+  $('.libraryHover').mouseleave(function() {
+    $('.option1').toggle();
+  
+  });
+});
