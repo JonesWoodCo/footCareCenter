1e7f2181c986eb085b0fd63bf2a192df3f6bd4ec
Topher <cjones.wingsofgold@gmail.com>
2015-03-31 13:24:10 -0500

    code cleanup

--- js/navDrop.js
+++ js/navDrop.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-         $('.option1').hide();
-
-
-  $('.libraryHover').mouseenter(function() {
-    $('.option1').toggle();
-  
-  });
-  $('.libraryHover').mouseleave(function() {
-    $('.option1').toggle();
-  
-  });
-});
+//$(document).ready(function(){
+//         $('.option1').hide();
+//
+//
+//  $('.libraryHover').mouseenter(function() {
+//    $('.option1').toggle();
+//  
+//  });
+//  $('.libraryHover').mouseleave(function() {
+//    $('.option1').toggle();
+//  
+//  });
+//});
